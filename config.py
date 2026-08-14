5e1306b85e02df90e7768b11a9afdfb2ce20fdc9
sarthakargade9166-max <sarthakargade9166@gmail.com>
2026-08-14 21:56:08 +0530

    bug fixes

--- config.py
+++ config.py
@@ -8,5 +8,6 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'nutristar-secure-default-key')
     GEMINI_API_KEY = os.getenv('GEMINI_API_KEY', '')
     DEBUG = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'
+    TEMPLATES_AUTO_RELOAD = True
     MAX_CONTENT_LENGTH = 1024 * 1024  # 1MB limit for request body
     JSON_SORT_KEYS = False
